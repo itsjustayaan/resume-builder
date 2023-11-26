6ba8117a47c6c82e2f74618b687ce2c1dc1c9468
Ayaan <ayaan2536@gmail.com>
2023-11-26 15:59:14 +0530

    fix

--- src/components/RightPanel/index.tsx
+++ src/components/RightPanel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { Paper } from "@mui/material";
 
 import type { Resume } from "../../data/resume";
@@ -6,18 +6,10 @@ import type { Resume } from "../../data/resume";
 import styles from "./index.module.css";
 
 function RightPanel(props: { resume: Resume }) {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    if (!iframe) return;
-  }, [props.resume]);
-
   return (
     <div className={styles.Div}>
       <Paper elevation={6} sx={styles.Paper}>
         {/* {JSON.stringify(props.resume, null, 2)} */}
-        <iframe ref={iframeRef} />
       </Paper>
     </div>
   );
